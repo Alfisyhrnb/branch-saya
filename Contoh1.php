a6bc537885d69a1c89c5666a0a4a0a7b9245b5ed
Alfisyhrnb <baihaqi1117@gmail.com>
2025-01-16 16:14:44 +0700

    alfibaihaqi

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "kenalkan";
+        
+        echo " nama saya alfi syahrin baihaqi tinggal di bogor";
+    }
+}
